060995e862a7259aba234da73ccd51307b595c33
Raúl Alejandro Verde Martínez <verde3ro@gmail.com>
2022-08-20 13:08:36 -0500

    Commit inicial

---
+++ src/main/java/com/appstracta/SpringBootSakilaApplication.java
@@ -0,0 +1,13 @@
+package com.appstracta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSakilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSakilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appstracta/SpringBootSakilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appstracta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSakilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
